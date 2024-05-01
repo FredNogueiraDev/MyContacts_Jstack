b28e6999674e2c266079a6646787a00a4459240c
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-05-01 19:08:45 -0300

    refactor: Refatoração da página Home e do Header

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,3 +1,89 @@
+import {
+  Container, InputSearchContainer, Header, ListContainer, Card,
+} from './styles';
+
+import arrow from '../../assets/images/arrow.svg';
+import edit from '../../assets/images/edit.svg';
+import trash from '../../assets/images/trash.svg';
+
 export default function Home() {
-  return 'Eu sou um alert!';
+  return (
+    <Container>
+      <InputSearchContainer>
+        <input type="text" placeholder="Pesquisar Contato..." />
+      </InputSearchContainer>
+
+      <Header>
+        <strong>3 contatos</strong>
+        <a href="/">Novo contato</a>
+      </Header>
+
+      <ListContainer>
+        <header>
+          <button type="button">
+            <span>Nome</span>
+            <img src={arrow} alt="Arrow" />
+          </button>
+        </header>
+
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Frederico Nogueira</strong>
+              <small>instagram</small>
+            </div>
+            <span>fredericonogueira01@outlook.com</span>
+            <span>(37) 99136-8237</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Frederico Nogueira</strong>
+              <small>instagram</small>
+            </div>
+            <span>fredericonogueira01@outlook.com</span>
+            <span>(37) 99136-8237</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+        <Card>
+          <div className="info">
+            <div className="contact-name">
+              <strong>Frederico Nogueira</strong>
+              <small>instagram</small>
+            </div>
+            <span>fredericonogueira01@outlook.com</span>
+            <span>(37) 99136-8237</span>
+          </div>
+
+          <div className="actions">
+            <a href="/">
+              <img src={edit} alt="Edit" />
+            </a>
+            <button type="button">
+              <img src={trash} alt="Delete" />
+            </button>
+          </div>
+        </Card>
+      </ListContainer>
+    </Container>
+  );
 }
