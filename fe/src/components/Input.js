89ccc4634b2e2e0953a7c537d71a93a766faf259
Frederico Nogueira <fredericonogueira01@outlook.com>
2024-05-02 06:45:44 -0300

    feat: Criado os componentes de Input e Select

---
+++ fe/src/components/Input.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components';
+
+export default styled.input`
+  width: 100%;
+  border: none;
+  background: ${({ theme }) => theme.color.gray.white};
+  border: 2px solid ${({ theme }) => theme.color.gray.white};
+  box-shadow: 0 4px 10px rgba(0, 0, 0, 0.04);
+  height: 52px;
+  border-radius: 4px;
+  outline: none;
+  padding: 0 16px;
+  font-size: 16px;
+
+  &:focus {
+    border-color: ${({ theme }) => theme.color.primary.main};
+  }
+`;
--- fe/src/pages/EditContact/index.js
+++ fe/src/pages/EditContact/index.js
@@ -1,3 +1,19 @@
+import PageHeader from '../../components/PageHeader';
+
+import Input from '../../components/Input';
+import Select from '../../components/Select';
+
 export default function EditContact() {
-  return 'EditContact';
+  return (
+    <>
+      <PageHeader title="Editar contato" />
+      <Input type="text" placeholder="Nome" />
+      <Select>
+        <option value="1">Instagram</option>
+        <option value="1">Facebook</option>
+        <option value="1">Linkedin</option>
+        <option value="1">WhatsApp</option>
+      </Select>
+    </>
+  );
 }
